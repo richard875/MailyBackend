11dee61446ca778369ed4d8957a22c030f359664
Richard Lee <richard_875@me.com>
2022-07-18 19:35:08 +1000

    - Modify recordController

--- src/controllers/recordController.go
+++ src/controllers/recordController.go
@@ -1,20 +1,21 @@
 package controllers
 
 import (
+	"github.com/joho/godotenv"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	ipdata "github.com/ipdata/go"
 	"gorm.io/gorm"
 
 	"maily/go-backend/src/models"
-	"maily/go-backend/src/secrets"
 
 	"github.com/aidarkhanov/nanoid"
 	"github.com/google/uuid"
 )
 
-// 123 godoc
+// Example 1 godoc
 // @Summary  1234
 // @Schemes
 // @Description  do ping
@@ -23,7 +24,6 @@ import (
 // @Produce      json
 // @Success      200  {string}  Helloworld
 // @Router       /example/12345 [get]
-
 func Beep(c *gin.Context) {
 	db := c.MustGet("DB").(*gorm.DB)
 	db.Create(&models.Record{UserID: uuid.New().String(), LogNumber: nanoid.New()})
@@ -40,14 +40,10 @@ func Beep(c *gin.Context) {
 // @Produce      json
 // @Success      200  {string}  Helloworld
 // @Router       /example/abcde [get]
-
 func IpAddress(c *gin.Context) {
-	ipd, _ := ipdata.NewClient(secrets.IpAddressApiKey)
-
-	data, err := ipd.Lookup("118.102.80.22")
-	if err != nil {
-		// handle error
-	}
+	_ = godotenv.Load(".env")
+	ipd, _ := ipdata.NewClient(os.Getenv("IP_ADDRESS_API_KEY"))
+	data, _ := ipd.Lookup("118.102.80.22")
 
 	c.IndentedJSON(http.StatusOK, data)
 }
