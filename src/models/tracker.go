3149242f3a821926608698011ddeb8270b6c26a2
Richard Lee <richard_875@me.com>
2022-11-23 00:42:48 +1100

    - Modify Tracker model

--- src/models/tracker.go
+++ src/models/tracker.go
@@ -8,5 +8,5 @@ type Tracker struct {
 	gorm.Model
 	ID                    string `gorm:"size:255;not null;unique;primary_key" json:"id"`        // public tracking number
 	PrivateTrackingNumber string `gorm:"size:255;not null;unique" json:"privateTrackingNumber"` // private tracking number
-	UserID                string `gorm:"type:char(36); foreignKey:UserRefer" json:"userId"`
+	UserID                string `gorm:"type:char(36);not null;foreignKey:UserRefer" json:"userId"`
 }
