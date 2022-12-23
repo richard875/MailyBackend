7854e6b0711ae15c7aae7001900d9074edd3c588
Richard Lee <richard_875@me.com>
2022-12-24 00:17:49 +1100

    - Move Beep function into logic file

--- src/controllers/recordController.go
+++ src/controllers/recordController.go
@@ -29,42 +29,16 @@ import (
 // @Success      200  {string}  Helloworld
 // @Router       /example/12345 [get]
 func Beep(c *gin.Context) {
-	db := c.MustGet("DB").(*gorm.DB)
-	_ = godotenv.Load(".env")
-	ipd, _ := ipdata.NewClient(os.Getenv("IP_ADDRESS_API_KEY"))
-	jsonFile, err := os.Open("static/data/user_agents_email_client.json")
+	err := logic.LogEmailOpen(c)
 	if err != nil {
 		utils.HandleError(c, err)
 		return
 	}
-	var userAgents []string // List of browser user agents
-	byteValue, _ := io.ReadAll(jsonFile)
-	json.Unmarshal(byteValue, &userAgents)
-	jsonFile.Close()
-
-	var tracker models.Record
-	// ipAddress := c.ClientIP()
-	ipAddress := "118.102.80.22"
-	userAgent := c.Request.Header.Get("User-Agent")
-	data, _ := ipd.Lookup(ipAddress) // Get IP address data
-
-	tracker.ID = uuid.New()
-	tracker.PublicTrackingNumber = c.Param("trackingId")
-	tracker.IpAddress = ipAddress
-	tracker.IpCity = data.City
-	tracker.IpCountry = data.CountryName
-	tracker.IsEU = data.IsEU
-	tracker.IsTor = data.Threat.IsTOR
-	tracker.IsProxy = data.Threat.IsProxy
-	tracker.IsAnonymous = data.Threat.IsAnonymous
-	tracker.IsKnownAttacker = data.Threat.IsKnownAttacker
-	tracker.IsKnownAbuser = data.Threat.IsKnownAbuser
-	tracker.IsThreat = data.Threat.IsThreat
-	tracker.IsBogon = data.Threat.IsBogon
-	tracker.ConfidentWithEmailClient = slices.IndexFunc(userAgents, func(agent string) bool { return agent == userAgent }) != -1
-	db.Create(&tracker)
 
 	c.File("static/images/beep.gif")
+}
+
+
 }
 
 func IpAddress(c *gin.Context) {
--- src/logic/recordLogic.go
+++ src/logic/recordLogic.go
@@ -14,3 +14,58 @@ import (
 	"maily/go-backend/src/models"
 	"os"
 )
+
+func LogEmailOpen(c *gin.Context) error {
+	// Setup IP client, load .env and user agent files
+	db := c.MustGet("DB").(*gorm.DB)
+	_ = godotenv.Load(".env")
+	ipd, _ := ipdata.NewClient(os.Getenv("IP_ADDRESS_API_KEY"))
+	jsonFile, openFileError := os.Open("static/data/user_agents_email_client.json")
+	if openFileError != nil {
+		return openFileError
+	}
+	var userAgents []string // List of browser user agents
+	byteValue, _ := io.ReadAll(jsonFile)
+	jsonUnmarshalError := json.Unmarshal(byteValue, &userAgents)
+	if jsonUnmarshalError != nil {
+		return jsonUnmarshalError
+	}
+	closeFileError := jsonFile.Close()
+	if closeFileError != nil {
+		return closeFileError
+	}
+
+	// Gather data for tracker
+	trackingNumber := c.Param("trackingId")
+	// ipAddress := c.ClientIP()
+	ipAddress := "118.102.80.22"     // Mock IP data
+	data, _ := ipd.Lookup(ipAddress) // Get IP address data
+	userAgent := c.Request.Header.Get("User-Agent")
+	confidentWithEmailClient := slices.IndexFunc(userAgents, func(agent string) bool { return agent == userAgent }) != -1
+
+	tracker := createTracker(data, trackingNumber, ipAddress, confidentWithEmailClient)
+	db.Create(&tracker)
+
+	return nil
+}
+
+func createTracker(ipData ipdata.IP, trackingNumber string, ipAddress string, confidentWithEmailClient bool) models.Record {
+	var tracker models.Record
+
+	tracker.ID = uuid.New()
+	tracker.PublicTrackingNumber = trackingNumber
+	tracker.IpAddress = ipAddress
+	tracker.IpCity = ipData.City
+	tracker.IpCountry = ipData.CountryName
+	tracker.IsEU = ipData.IsEU
+	tracker.IsTor = ipData.Threat.IsTOR
+	tracker.IsProxy = ipData.Threat.IsProxy
+	tracker.IsAnonymous = ipData.Threat.IsAnonymous
+	tracker.IsKnownAttacker = ipData.Threat.IsKnownAttacker
+	tracker.IsKnownAbuser = ipData.Threat.IsKnownAbuser
+	tracker.IsThreat = ipData.Threat.IsThreat
+	tracker.IsBogon = ipData.Threat.IsBogon
+	tracker.ConfidentWithEmailClient = confidentWithEmailClient
+
+	return tracker
+}
