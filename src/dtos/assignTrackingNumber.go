094caf01daffa1f93e256f556d06a6315ee1e0a2
Richard Lee <richard_875@me.com>
2023-01-01 16:33:20 +1100

    - Add ComposeAction, Subject, FromAddress, ToAddresses, CcAddresses, BccAddresses, ReplyToAddresses and InternalMessageID to Tracker database model and Tracker DTO
    - Add TimesOpened to Tracker database model

--- src/dtos/assignTrackingNumber.go
+++ src/dtos/assignTrackingNumber.go
@@ -1,5 +1,13 @@
 package dtos
 
 type TrackingNumber struct {
-	TrackingNumber string `json:"trackingNumber" binding:"required"`
+	TrackingNumber    string `json:"trackingNumber" binding:"required"`
+	ComposeAction     int    `json:"composeAction" binding:"required"`
+	Subject           string `json:"subject" binding:"required"`
+	FromAddress       string `json:"fromAddress" binding:"required"`
+	ToAddresses       string `json:"toAddresses" binding:"required"`
+	CcAddresses       string `json:"ccAddresses" binding:"required"`
+	BccAddresses      string `json:"bccAddresses" binding:"required"`
+	ReplyToAddresses  string `json:"replyToAddresses" binding:"required"`
+	InternalMessageID string `json:"internalMessageID" binding:"required"`
 }
--- src/models/tracker.go
+++ src/models/tracker.go
@@ -6,6 +6,15 @@ import (
 
 type Tracker struct {
 	gorm.Model
-	ID     string `gorm:"size:255;not null;unique;primary_key" json:"id"` // public tracking number
-	UserID string `gorm:"type:char(36);not null;foreignKey:UserRefer" json:"userId"`
+	ID                string `gorm:"size:255;not null;unique;primary_key" json:"id"` // public tracking number
+	UserID            string `gorm:"type:char(36);not null;foreignKey:UserRefer" json:"userId"`
+	TimesOpened       int    `gorm:"type:int;not null;default:0" json:"timesOpened"`
+	ComposeAction     int    `gorm:"type:int;not null" json:"composeAction"` // newMessage: 1, reply: 2, replyAll: 3, forward: 4
+	Subject           string `gorm:"type:varchar(255);not null" json:"subject"`
+	FromAddress       string `gorm:"type:varchar(255);not null" json:"fromAddress"`
+	ToAddresses       string `gorm:"type:varchar(255);not null" json:"toAddresses"`
+	CcAddresses       string `gorm:"type:varchar(255);not null" json:"ccAddresses"`
+	BccAddresses      string `gorm:"type:varchar(255);not null" json:"bccAddresses"`
+	ReplyToAddresses  string `gorm:"type:varchar(255);not null" json:"replyToAddresses"`
+	InternalMessageID string `gorm:"type:varchar(255);not null" json:"internalMessageID"` // No computational use, just for reference
 }
