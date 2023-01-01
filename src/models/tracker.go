95a7f06769ced7265a1bde7f8df65d54486c5ccd
Richard Lee <richard_875@me.com>
2023-01-01 23:57:19 +1100

    - Add default value to "compose_action"

--- src/models/tracker.go
+++ src/models/tracker.go
@@ -9,7 +9,7 @@ type Tracker struct {
 	ID                string `gorm:"size:255;not null;unique;primary_key" json:"id"` // public tracking number
 	UserID            string `gorm:"type:char(36);not null;foreignKey:UserRefer" json:"userId"`
 	TimesOpened       int    `gorm:"type:int;not null;default:0" json:"timesOpened"`
-	ComposeAction     int    `gorm:"type:int;not null" json:"composeAction"` // newMessage: 1, reply: 2, replyAll: 3, forward: 4
+	ComposeAction     int    `gorm:"type:int;not null;default:-1" json:"composeAction"` // newMessage: 1, reply: 2, replyAll: 3, forward: 4
 	Subject           string `gorm:"type:varchar(255);not null" json:"subject"`
 	FromAddress       string `gorm:"type:varchar(255);not null" json:"fromAddress"`
 	ToAddresses       string `gorm:"type:varchar(255);not null" json:"toAddresses"`
