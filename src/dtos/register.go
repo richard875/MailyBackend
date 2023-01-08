584152be8592378775a1432c921fd5e45bc0bf8b
Richard Lee <richard_875@me.com>
2023-01-08 17:10:32 +1100

    - Add FirstName and LastName to Register DTO

--- src/dtos/register.go
+++ src/dtos/register.go
@@ -1,6 +1,8 @@
 package dtos
 
 type Register struct {
-	Email    string `json:"email" binding:"required"`
-	Password string `json:"password" binding:"required"`
+	FirstName string `json:"firstName" binding:"required"`
+	LastName  string `json:"lastName" binding:"required"`
+	Email     string `json:"email" binding:"required"`
+	Password  string `json:"password" binding:"required"`
 }
