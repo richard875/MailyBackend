bc5257ec9776c1ca13b1cefefccccdd785baee59
Richard Lee <richard_875@me.com>
2023-01-08 17:10:43 +1100

    - Add FirstName and LastName to Register model

--- src/models/user.go
+++ src/models/user.go
@@ -8,6 +8,8 @@ import (
 type User struct {
 	gorm.Model
 	ID            uuid.UUID `gorm:"type:char(36);not null;unique;primary_key" json:"id"`
+	FirstName     string    `gorm:"type:varchar(255);not null" json:"firstName"`
+	LastName      string    `gorm:"type:varchar(255);not null" json:"lastName"`
 	Email         string    `gorm:"size:255;not null" json:"email"`
 	Password      string    `gorm:"size:255;not null" json:"password"`
 	EmailVerified bool      `gorm:"default:true" json:"emailVerified"`
