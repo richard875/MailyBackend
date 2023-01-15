c0e6801c24f2b05876c63fb1540464dd3a1e0154
Richard Lee <richard_875@me.com>
2023-01-15 15:17:44 +1100

    - Bug: Do not modify "updated_at" column on update

--- src/logic/recordLogic.go
+++ src/logic/recordLogic.go
@@ -172,7 +172,7 @@ func GetTrackerClicks(c *gin.Context) ([]models.Record, error) {
 	}
 
 	// Update tracker
-	db.Model(&currentTracker).Update("Updated", false)
+	db.Model(&currentTracker).UpdateColumn("Updated", false)
 
 	var records []models.Record
 	err := db.Order(fmt.Sprintf("created_at %s", sortDirection)).Where("public_tracking_number = ?", trackingNumber).Find(&records).Error
