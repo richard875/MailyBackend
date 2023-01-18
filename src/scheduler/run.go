fad816698847246e51665c14afe8b00e9d3a6b51
Richard Lee <richard_875@me.com>
2023-01-18 22:50:05 +1100

    - Refactor: Separate createFulltextIndex run function to a new file

---
+++ src/scheduler/run.go
@@ -0,0 +1,18 @@
+package scheduler
+
+import (
+	"log"
+	"maily/go-backend/src/database"
+	"time"
+)
+
+func Run() {
+	// Run the createFulltextIndex function every 24 hours
+	ticker := time.NewTicker(time.Hour * 24)
+	go func() {
+		for range ticker.C {
+			log.Println("Running create fulltext index")
+			FulltextIndex(database.DB)
+		}
+	}()
+}
