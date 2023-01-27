c2fa335bdc03e050d69ae91f8547d7884ebf4c85
Richard Lee <richard_875@me.com>
2023-01-27 23:10:03 +1100

    - Big: Modify timezone

--- src/telegramBot/notify.go
+++ src/telegramBot/notify.go
@@ -4,17 +4,16 @@ import (
 	"fmt"
 	TelegramBotAPI "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"maily/go-backend/src/models"
-	"time"
 )
 
 func NotifyUser(userID int64, emailSubject string, record models.Record) {
-	timeZone, _ := time.LoadLocation("America/New_York")
+	//timeZone, _ := time.LoadLocation("America/New_York")
 	// Create message
 	location := record.IpCountry
 	if record.IpCity != "" {
 		location = fmt.Sprintf("%s, %s", record.IpCity, record.IpCountry)
 	}
-	message := fmt.Sprintf(notifyMessage, location, record.EmojiFlag, emailSubject, record.IpAddress, record.CreatedAt.In(timeZone).Format("02 Jan 2006, 15:04:05 PM"))
+	message := fmt.Sprintf(notifyMessage, location, record.EmojiFlag, emailSubject, record.IpAddress, record.CreatedAt.Format("02 Jan 2006, 15:04:05 PM"))
 
 	msg := TelegramBotAPI.NewMessage(userID, message)
 	msg.ParseMode = "Markdown"
